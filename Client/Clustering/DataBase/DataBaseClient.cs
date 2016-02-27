4494b37edc7b8b7c531587dbdf2900ce7d3a9e67
Sergey <sergey.suzdalskiy@gmail.com>
2016-02-27 19:32:35 +0200

    Connect to DB

---
+++ Client/Clustering/DataBase/DataBaseClient.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace Clustering.DB
+{
+    public static class DataBaseClient
+    {
+        private static string Port = "27017";
+        private static string IP = "192.168.0.103";
+
+        public static void Init()
+        {
+            var client = new MongoClient("mongodb://" + IP + ":" + Port);
+
+            var database = client.GetDatabase("Clustering");
+
+            var collection = database.GetCollection<BsonDocument>("myCollection");
+            
+        }
+    }
+}
--- Client/Clustering/MainWindow.xaml.cs
+++ Client/Clustering/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Clustering.DB;
 
 namespace Clustering
 {
@@ -23,6 +24,8 @@ namespace Clustering
         public MainWindow()
         {
             InitializeComponent();
+
+            DataBaseClient.Init();
         }
     }
 }
