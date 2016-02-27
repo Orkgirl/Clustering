9a4899ff87ff2711d1bc5aee73b9a7b14334a8de
Sergey <sergey.suzdalskiy@gmail.com>
2016-02-27 19:38:48 +0200

    Fix namespace

--- Client/Clustering/DataBase/DataBaseClient.cs
+++ Client/Clustering/DataBase/DataBaseClient.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using MongoDB.Bson;
+﻿using MongoDB.Bson;
 using MongoDB.Driver;
 
-namespace Clustering.DB
+namespace Clustering.DataBase
 {
     public static class DataBaseClient
     {
