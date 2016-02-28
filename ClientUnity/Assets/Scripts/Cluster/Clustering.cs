4c6d8940b18a2e0a98af4948721ccda6f307eaad
Elizabeth Suzdalska <Elizabeth Suzdalska>
2016-02-28 23:53:35 +0200

    added normalize function

--- ClientUnity/Assets/Scripts/Cluster/Clustering.cs
+++ ClientUnity/Assets/Scripts/Cluster/Clustering.cs
@@ -23,11 +23,45 @@ public static class Clustering
     public static void Init(ClusterMap mapData)
     {
         _origCluster = mapData;
+        _normalizeCluster = Normolize(_origCluster);
     }
 
     public static ClusterMap Normolize(ClusterMap orig)
     {
        var result = new ClusterMap();
+        List<ClusterColumn> normalizeList = new List<ClusterColumn>();
+        float originalValue = 0;
+        float averageQuadratic = 0;
+        float sampleMean = 0;
+        float normaliseValue;
+
+        var keys = orig.Columns.Keys;
+        foreach(var key in keys)
+        {
+            List<ClusterColumn> columns;
+            if(orig.Columns.TryGetValue(key, out columns))
+            {
+                float sumOfAllValues = 0;
+                for (int i = 0; i < columns.Count; i++)
+                {                    
+                    sumOfAllValues += columns[i].Value;
+                }
+                sampleMean = (1 / columns.Count) * sumOfAllValues;
+
+                float sumOfDifferenceCurrentSample = 0;
+                for (int i = 0; i < columns.Count; i++)
+                {
+                    sumOfDifferenceCurrentSample += (columns[i].Value - sampleMean) * (columns[i].Value - sampleMean);
+                }
+                averageQuadratic = (float) Math.Sqrt((1 / (columns.Count - 1)) * sumOfDifferenceCurrentSample);
+            }
+            else
+            {
+                UnityEngine.Debug.LogError("!!!!!!!!!!!!");
+            }
+        }
+               
+        normaliseValue = (originalValue - sampleMean) / averageQuadratic;
 
         return result;
     }
