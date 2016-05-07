1e5349af62e09b41aec317a52be5f2603bb58023
Sergey <sergey.suzdalskiy@gmail.com>
2016-05-07 23:50:40 +0300

    Update Indicator Select

---
+++ ClientUnity/Assets/Scripts/UI/Indicator/IndicatorItem.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IndicatorItem : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void ClicItemHendler()
+    {
+        Debug.Log("ffffffffffff");
+    }
+}
--- ClientUnity/Assets/Scripts/UI/IndicatorSelection.cs
+++ ClientUnity/Assets/Scripts/UI/IndicatorSelection.cs
@@ -2,9 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using UnityEngine;
 
 public class IndicatorSelection : UIItem
 {
+    [SerializeField]
+    private IndicatorItem _itemPrefab;
 
+    [SerializeField]
+    private GameObject _selectedContent;
+
+    [SerializeField]
+    private GameObject _allContent;
 }
 
