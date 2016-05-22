ee2f81149157bf84f05fdd8266c33c80d792d370
RocketMaiden <elizabeth.ruchko@gmail.com>
2016-05-23 00:19:38 +0300

    add bin to ignore

---
+++ Diploma_Fake/Assets/Scripts/ToggleParent.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using UnityEngine.EventSystems;
+using System;
+
+public class ToggleParent : Toggle {
+
+    public Guid Id;
+    public Text Label;
+    public List<Toggle> child = new List<Toggle>();
+
+    
+    public override void OnPointerClick(PointerEventData eventData)
+    {
+        base.OnPointerClick(eventData);
+        
+        foreach(var item in child)
+        {
+            item.isOn = this.isOn;
+        }
+    }
+}
