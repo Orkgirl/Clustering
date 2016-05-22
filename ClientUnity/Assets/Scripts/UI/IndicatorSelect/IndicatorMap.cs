b4d354c365eda90be4d13a9cfb646e20eacc406e
RocketMaiden <elizabeth.ruchko@gmail.com>
2016-05-22 19:48:54 +0300

    Add new Indicator select window

---
+++ ClientUnity/Assets/Scripts/UI/IndicatorSelect/IndicatorMap.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System;
+
+public class IndicatorNoda
+{
+    public IndicatorNoda Parent;    
+    public List<IndicatorNoda> Child;
+    public IndicatorNoda()
+    {
+        Id = Guid.NewGuid();
+    }
+    
+    public Guid Id;    
+    public string Name;    
+    public bool DefaultState;
+}
