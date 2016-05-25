4072cb442dd78b8115761eb70465fc2131ffa882
RocketMaiden <elizabeth.ruchko@gmail.com>
2016-05-25 19:32:13 +0300

    add new cluster window, map window

---
+++ Diploma_Fake/Assets/Scripts/ImputNo.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ImputNo : MonoBehaviour {
+
+    public InputField Input;
+    public int CurrentValue;
+    public int MaxValue;
+    public int MinValue;
+    public int ValueStep;
+
+	public void OnClickHandler (int value)
+    {
+        CurrentValue = CurrentValue + value * ValueStep;
+        if (CurrentValue < MinValue)
+        {
+            CurrentValue = MinValue;
+        }
+        if (CurrentValue > MaxValue)
+        {
+            CurrentValue = MaxValue;
+        }
+
+        Input.text = CurrentValue.ToString();
+    }
+
+    void Start()
+    {
+        CurrentValue = MinValue;
+        Input.text = CurrentValue.ToString();
+    }
+}
